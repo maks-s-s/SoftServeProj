6ed553dc817934939c197f2b0d2a1dec3c6ddce1
Hermoonumu <geron9988@gmail.com>
2025-05-02 13:28:55 +0300

    Completely revamped backend

--- src/main/java/com/softserve/academy/ShoppingSystem.java
+++ src/main/java/com/softserve/academy/ShoppingSystem.java
@@ -1,25 +1,13 @@
 package com.softserve.academy;
 
-import lombok.AllArgsConstructor;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-@AllArgsConstructor
-
-public class ShoppingSystem implements CommandLineRunner {
-
+public class ShoppingSystem {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShoppingSystem.class, args);
+        SpringApplication.run(com.softserve.academy.ShoppingSystem.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Starting application...");
-
-    }
 }
