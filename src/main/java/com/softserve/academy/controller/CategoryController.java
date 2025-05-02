91796ff17a007c864539b461f7ba6d53e18af2ae
Kochubeinyk_DMytro <kochubeynikdima@gmail.com>
2025-05-02 23:21:50 +0300

    post-merge

---
+++ src/main/java/com/softserve/academy/controller/CategoryController.java
@@ -0,0 +1,32 @@
+package com.softserve.academy.controller;
+
+import com.softserve.academy.model.Category;
+import com.softserve.academy.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/categories")
+public class CategoryController {
+    public final CategoryService categoryService;
+    @Autowired
+    public CategoryController( CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/categories/add")
+    public void addCategory(@RequestBody Category category) {
+        categoryService.addCategory(category);
+    }
+
+    @GetMapping("/categories/findAll")
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.findAllCategories());
+
+    }
+}
