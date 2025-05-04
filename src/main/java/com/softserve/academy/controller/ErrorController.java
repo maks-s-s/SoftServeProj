d7f201c57dd6e368b9fd4314b5d5ced85157d457
Максим Назаренко <maksimnazarenko779@gmail.com>
2025-05-04 14:13:37 +0300

    fix

--- src/main/java/com/softserve/academy/controller/ErrorController.java
+++ src/main/java/com/softserve/academy/controller/ErrorController.java
@@ -1,18 +1,26 @@
 package com.softserve.academy.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ErrorController {
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
     @RequestMapping("/error")
-    public String handleError(Model model) {
-        Integer statusCode = (Integer) model.asMap().get("javax.servlet.error.status_code");
+    public String handleError(HttpServletRequest request, HttpServletResponse response, Model model) {
+        Integer statusCode = response.getStatus();
+
+        System.out.println("Error Code: " + statusCode);
+
+        if (statusCode == 0) {
+            statusCode = 500;
+        }
 
         model.addAttribute("errorCode", statusCode);
 
-        return "error";
+        return "errors/error";
     }
 }
